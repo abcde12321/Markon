7ef75c6a21d26decae21169412d1898168094d23
Fan <cyangfan@gmail.com>
2024-08-08 10:14:29 +0200

    Initial Commit

---
+++ Markon/MarkonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkonApp.swift
+//  Markon
+//
+//  Created by Fan Yang on 08/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MarkonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
